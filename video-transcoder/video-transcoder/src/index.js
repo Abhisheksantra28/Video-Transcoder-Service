f01e729d0aedfb1fab202780de56d001be2d3712
Abhishek Santra <abhisheksantra28@gmail.com>
2024-02-28 23:24:37 +0530

    Setup Files for video-transcoder

---
+++ video-transcoder/video-transcoder/src/index.js
@@ -0,0 +1,22 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+
+app.use(express.json())
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
