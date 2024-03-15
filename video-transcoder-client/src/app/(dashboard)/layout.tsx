309b1be008e89dac18333e1b98b05a7b9a119850
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-15 19:22:16 +0530

    Modify Layout.tsx

--- video-transcoder-client/src/app/(dashboard)/layout.tsx
+++ video-transcoder-client/src/app/(dashboard)/layout.tsx
@@ -1,14 +1,15 @@
+import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen relative ">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
+    <div className="flex flex-col h-screen relative w-full overflow-hidden ">
+      <Header />
+      <main className="flex-1 flex">
         <Sidebar />
-      </div>
-      {/* <main className="md:pl-72 ">{children}</main> */}
-     <div className="md:pl-72 h-full bg-[#f3f4f6]">{children}</div>
+        {children}
+      </main>
     </div>
   );
 };
