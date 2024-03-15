e96f9f17e7eb477cc283e0866a77b69c6a775180
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-15 19:23:11 +0530

    Add Navbar

---
+++ video-transcoder-client/src/components/Header.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <div className="w-full">
+      <header className="flex h-16 items-center justify-between  border-b border-gray-200 px-4 md:px-6 dark:border-gray-800">
+        <Link className="text-[1.6rem] font-bold" href="/">
+          Transcoder
+        </Link>
+
+        <div className="flex gap-4">
+          <Button
+            className="text-lg hover:bg-gray-800 hover:text-white transition-all duration-300"
+            variant="secondary"
+          >
+            SignIn
+          </Button>
+
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>U</AvatarFallback>
+          </Avatar>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
