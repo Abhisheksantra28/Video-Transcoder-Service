27591ae06f0e2b8ed30ec3d31a107f0c992524c1
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-17 10:42:23 +0530

    Added middlewares

---
+++ video-transcoder-server/src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+const { User } = require("../models/user.model.js");
+const { ApiError } = require("../utils/ApiError.js");
+const { asyncHandler } = require("../utils/asyncHandler.js");
+const Jwt = require("jsonwebtoken");
+
+const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const decodedToken = Jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
+
+module.exports = {
+  verifyJWT,
+};
