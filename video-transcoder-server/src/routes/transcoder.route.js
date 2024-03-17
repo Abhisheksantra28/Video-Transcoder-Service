4d4c3c74108b8079eeb7cb705d0243611b8667c4
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-18 00:24:23 +0530

    Add middleware

--- video-transcoder-server/src/routes/transcoder.route.js
+++ video-transcoder-server/src/routes/transcoder.route.js
@@ -3,10 +3,11 @@ const {
   handleECSTrigger,
 } = require("../controllers/transcoder.controller.js");
 const { Router } = require("express");
+const { verifyJWT } = require("../middlewares/auth.middleware.js");
 
 const router = Router();
 
-router.post("/s3-trigger", handleS3Trigger);
+router.post("/s3-trigger",verifyJWT, handleS3Trigger);
 router.post("/ecs-trigger", handleECSTrigger);
 
 module.exports = router;
