47c0f9dba36d51651c7d246648d87711aa5acac0
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-18 00:23:15 +0530

    Modify index.js

--- video-transcoder-server/src/index.js
+++ video-transcoder-server/src/index.js
@@ -1,9 +1,28 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const transcoderRoutes = require("./routes/transcoder.route.js");
+const userRoutes = require("./routes/user.route.js");
+const cookieParser = require("cookie-parser");
+const { connectDB } = require("./db/database.js");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error:", error);
+      throw error;
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection Failed !!", err);
+  });
+
+
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
@@ -18,6 +37,7 @@ app.get("/path", (req, res) => {
 });
 
 app.use("/api/v1/video", transcoderRoutes);
+app.use("/api/v1/user", userRoutes);
 
 app.use((req, res, next) => {
   return res.status(404).json({
