d82538191f8f35ba3eaa959d71eb080b393eb3df
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-17 23:35:35 +0530

    Fix User import

--- video-transcoder-server/src/middlewares/auth.middleware.js
+++ video-transcoder-server/src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-const { User } = require("../models/user.model.js");
+const  User  = require("../models/user.model.js");
 const { ApiError } = require("../utils/ApiError.js");
 const { asyncHandler } = require("../utils/asyncHandler.js");
 const Jwt = require("jsonwebtoken");
@@ -9,12 +9,14 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
+
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
 
     const decodedToken = Jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
+  
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
     );
