daec5473cfaf33a1ab700dd76d3667146b8367b8
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-20 15:18:17 +0530

    Fix login url

--- video-transcoder-server/src/routes/user.route.js
+++ video-transcoder-server/src/routes/user.route.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const passport = require("passport");
-const { getCurrentUser, logoutUser } = require("../controllers/user.controller.js");
+const {
+  getCurrentUser,
+  logoutUser,
+} = require("../controllers/user.controller.js");
 const { isAuthenticated } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
@@ -8,15 +11,16 @@ const router = express.Router();
 router.get(
   "/google-login",
   passport.authenticate("google", {
-    scope: ["profile"],
-  }),
-  (req, res, next) => {
-    // Handle successful authentication (e.g., redirect to frontend)
-    if (!req.user) {
-      return next(new Error("Authentication failed")); // Example error handling
-    }
-    res.redirect(process.env.FRONTEND_URL);
-  }
+    scope: ["profile", "email"],
+  })
+);
+
+router.get(
+  "/login",
+  passport.authenticate("google", {
+    successRedirect: `${process.env.FRONTEND_URL}/upload`,
+    failureRedirect: `${process.env.FRONTEND_URL}/signin`,
+  })
 );
 
 router.get("/current-user", isAuthenticated, getCurrentUser);
