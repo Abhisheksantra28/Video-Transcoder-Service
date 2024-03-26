cd38d5c95de5afa417fc90520f2769486c6564a0
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-26 15:42:04 +0530

    Add signin and signout functionalities

--- video-transcoder-client/src/app/(dashboard)/layout.tsx
+++ video-transcoder-client/src/app/(dashboard)/layout.tsx
@@ -1,24 +1,36 @@
 "use client";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
-import { RootState } from "@/redux/reducer/userReducer";
-import React from "react";
-import { useSelector } from "react-redux";
+import {
+  RootState,
+  userExist,
+  userNotExist,
+} from "@/redux/reducer/userReducer";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  const { user, loader } = useSelector(
-    (state: RootState) => state.userReducer
-  );
-  let isLoggedIn = false;
-  if (user != null) {
-    isLoggedIn = true;
-  } else {
-    isLoggedIn = false;
-  }
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://ecezbkpsc5.execute-api.ap-south-1.amazonaws.com/api/v1/user/current-user`,
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => dispatch(userExist(res.data.data)))
+      .catch((err) => dispatch(userNotExist()));
+  }, [dispatch]);
+
+  const { user } = useSelector((state: RootState) => state.userReducer);
+
   return (
     <div className="flex flex-col h-screen relative w-full overflow-hidden ">
-      <Header isLoggedIn={isLoggedIn} loader={loader} />
-   
+      <Header user={user} />
+
       <main className="flex-1 flex">
         <Sidebar />
         {children}
--- video-transcoder-client/src/app/page.tsx
+++ video-transcoder-client/src/app/page.tsx
@@ -1,102 +1,38 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { RootState, userExist, userNotExist } from "@/redux/reducer/userReducer";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import React, { useState } from "react";
-
-interface UploadState {
-  selectedFile: File | null;
-  error: string | null;
-}
-
-const UploadPage: React.FC = () => {
-  const [files, setFiles] = useState<UploadState>({
-    selectedFile: null,
-    error: null,
-  });
-
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const uploadFiles = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-
-    if (!file) return;
-    // Check if file is a video
-    if (!file.type.startsWith("video/")) {
-      setFiles({ ...files, error: "Only video files are allowed!" });
-      return;
-    }
-
-    setFiles({ ...files, error: null, selectedFile: file });
-  };
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const { selectedFile } = files;
-
-    if (!selectedFile) return;
-
-    try {
-      setLoading(true);
-      const { name, type } = selectedFile;
-      const res = await fetch(
-        `/api/upload?fileName=${name}&contentType=${type}`
-      );
-      const { url } = await res.json();
-
-      console.log(url);
-
-      const upload = await fetch(url, {
-        method: "PUT",
-        body: selectedFile,
-        headers: {
-          "Content-Type": type!,
-        },
-      });
-
-      if (upload.ok) {
-        console.log("Uploaded successfully!");
-      } else {
-        console.error("Upload failed.");
-      }
-      setLoading(false);
-    } catch (error) {
-      console.error("Error occurred during upload:", error);
-      setLoading(false);
-    }
-
-    setFiles({ ...files, selectedFile: null });
-  };
+const page = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios
+      .get(
+        `https://ecezbkpsc5.execute-api.ap-south-1.amazonaws.com/api/v1/user/current-user`,
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => dispatch(userExist(res.data.data)))
+      .catch((err) => dispatch(userNotExist()));
+  }, [dispatch]);
+  const { isAuthenticated } = useSelector(
+    (state: RootState) => state.userReducer
+  );
 
   return (
-    <div className="container h-screen overflow-y-hidden mx-auto px-4 py-8 flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-6">Upload Video</h1>
-      {files.error && <p className="text-red-500">{files.error}</p>}
-      <form onSubmit={handleSubmit} method="POST">
-        <label
-          htmlFor="file-upload"
-          className="block cursor-pointer mb-4 text-gray-700 font-bold"
-        >
-          Select Video:
-          <input
-            id="file-upload"
-            type="file"
-            className="hidden"
-            onChange={uploadFiles}
-          />
-        </label>
-        {files.selectedFile && (
-          <p className="text-gray-500">{files.selectedFile.name}</p>
-        )}
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          disabled={loading}
-        >
-          Upload
-        </button>
-      </form>
+    <div className="h-screen flex items-center justify-center">
+      {isAuthenticated ? (
+        <Button onClick={() => router.push("/upload")}>Get started</Button>
+      ) : (
+        <Button onClick={() => router.push("/signin")}>Get started</Button>
+      )}
     </div>
   );
 };
 
-export default UploadPage;
+export default page;
