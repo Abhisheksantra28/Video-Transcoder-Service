51e7c84ae79f35792cbdc8df324c9ac54ac340f2
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-29 00:09:58 +0530

    Fixes route for videoStatus endpoint

--- video-transcoder-server/src/routes/transcoder.route.js
+++ video-transcoder-server/src/routes/transcoder.route.js
@@ -3,14 +3,19 @@ const {
   handleECSTrigger,
 } = require("../controllers/transcoder.controller.js");
 const { Router } = require("express");
-const { getAllVideos, getVideoByFileName } = require("../controllers/video.contoller.js");
+const {
+  getAllVideos,
+  getVideoByFileName,
+  getVideoStatus,
+} = require("../controllers/video.contoller.js");
 
 const router = Router();
 
 router.post("/s3-trigger", handleS3Trigger);
 router.post("/ecs-trigger", handleECSTrigger);
 
-router.get("/get-all-videos",  getAllVideos);
-router.get("/v/:fileName",  getVideoByFileName);
+router.get("/get-all-videos", getAllVideos);
+router.get("/v/:fileName", getVideoByFileName);
+router.get("/s/:videoId", getVideoStatus);
 
 module.exports = router;
