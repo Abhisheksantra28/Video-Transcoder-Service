af44b67860fd5bdead645491dbc282d5e45aedcc
Abhishek Santra <abhisheksantra28@gmail.com>
2024-03-31 16:30:48 +0530

    Add settings page

---
+++ video-transcoder-client/src/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen'>
+        <div className="w-full p-8">
+        <h1 className="text-3xl font-bold mb-4">Settings</h1>
+        </div>
+    </div>
+  )
+}
+
+export default page
