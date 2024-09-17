12dceeee75aed63b7ad383afaa87b2477c00b220
Abhishek Santra <abhisheksantra28@gmail.com>
2024-09-18 00:08:57 +0530

    Add s3 controllers

--- server/src/controllers/s3.controlllers.js
+++ server/src/controllers/s3.controlllers.js
@@ -1,11 +1,12 @@
-const { putObjectURL } = require("../utils/s3SignedUrl.js");
+const { ApiResponse } = require("../utils/ApiResponse.js");
+const { putObjectURL, getObjectURL } = require("../utils/s3SignedUrl.js");
 
-const uploadToTempBucketURL = (req, res) => {
-  const { fileName, contentType } = req.params;
+const uploadToTempBucketURL = async (req, res) => {
+  const { fileName, contentType } = req.query;
 
   const bucketName = process.env.TEMP_S3_BUCKET_NAME;
 
-  const url = putObjectURL(fileName, contentType, bucketName);
+  const url = await putObjectURL(fileName, contentType, bucketName);
 
   if (!url) {
     return res
@@ -18,9 +19,25 @@ const uploadToTempBucketURL = (req, res) => {
     .json(new ApiResponse(200, url, "Signed URL generated successfully"));
 };
 
+const getFinalBucketvideoURL = async () => {
+  const { fileName, videoUrl } = req.query;
 
+  const key = `videos/${fileName}/${videoUrl}`;
+  const bucketName = process.env.FINAL_S3_BUCKET_NAME;
 
+  const url = await getObjectURL(key, bucketName);
+  if (!url) {
+    return res
+      .status(500)
+      .json(new ApiResponse(500, {}, "Error while generating the signed URL"));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, url, "Signed URL generated successfully"));
+};
 
 module.exports = {
-  uploadToTempBucket,
+  uploadToTempBucketURL,
+  getFinalBucketvideoURL,
 };
